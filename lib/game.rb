ee3dc42820e1e0d1532bf118ab42e82d879ba0de
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-13 14:14:08 +0200

    refactored the commited code

--- lib/game.rb
+++ lib/game.rb
@@ -8,10 +8,12 @@ class Game
     shuffle_word = @answer.split("").shuffle.join
     # shuffle_word if shuffle_word != answer
 
-    if shuffle_word != answer
-      shuffle_word
-    else
-      "invalid"
-    end
+    # if shuffle_word != answer
+    #   shuffle_word
+    # else
+    #   "invalid"
+    # end
+
+    shuffle_word != answer ? shuffle_word : "invalid"
   end
 end
