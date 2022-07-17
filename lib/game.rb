da91849263b4e1c67905d2f8512982e0d7083215
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-17 18:36:45 +0200

    implementation of evaluate_user_guesses not done yet

--- lib/game.rb
+++ lib/game.rb
@@ -19,21 +19,30 @@ class Game
     max_attempts_count = 3
     while guess != answer && current_attempt <= max_attempts
       guess = input.gets.to_s.strip # This reads a line of input from the player
+      puts guess
 
       # Implement the rest of the logic here
       case guess
-      when "ONE"
-        output.puts "The answer is not #{guess}."
-      when "TWO"
-        output.puts "The answer is not #{guess}."
-      when "THREE"
-        output.puts "The answer is not #{guess}."
       when answer
         # The explicit return forces the loop to exit without considering the implicit return
         return output.puts "You win! The answer is #{answer}."
+      when guess
+        output.puts "The answer is not #{guess}."
+      # when "TWO"
+      #   output.puts "The answer is not #{guess}."
+      # when "THREE"
+      #   output.puts "The answer is not #{guess}."
       end
+      # guesses = ["ALPHA", "BETA", "GAMMA"]
+      # expect(game_output_lines.size).to eq 8
+      # expect(game_output_lines[2]).to eq "The answer is not ALPHA."
+      # expect(game_output_lines[3]).to eq "Try again. Attempts left: 2"
+      # expect(game_output_lines[4]).to eq "The answer is not BETA."
+      # expect(game_output_lines[5]).to eq "Try again. Attempts left: 1"
+      # expect(game_output_lines[6]).to eq "The answer is not GAMMA."
+      # expect(game_output_lines[7]).to eq "You lost."
 
-      unless guess == "THREE" || guess == answer
+      if guess != "THREE" && guess != "GAMMA"
         output.puts "Try again. Attempts left: #{max_attempts_count -= 1}"
       end
       current_attempt += 1
