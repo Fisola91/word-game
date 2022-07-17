5c30dc6e5af76412af35739145d8f3e17dc23747
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-17 21:11:34 +0200

    modifed the evaluate_user_guesses method

--- lib/game.rb
+++ lib/game.rb
@@ -19,16 +19,13 @@ class Game
     max_attempts_count = 3
     while guess != answer && current_attempt <= max_attempts
       guess = input.gets.to_s.strip # This reads a line of input from the player
-      puts guess
-
-      # Implement the rest of the logic here
-      case guess
-      when answer
+      if guess == answer
         return output.puts "You win! The answer is #{answer}."
-      when guess
+      else
         output.puts "The answer is not #{guess}."
       end
-     if guess != answer && current_attempt < max_attempts
+
+      if guess != answer && current_attempt < max_attempts
         output.puts "Try again. Attempts left: #{max_attempts_count -= 1}"
       end
       current_attempt += 1
