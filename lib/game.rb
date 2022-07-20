630bdeb316f5f0de196344cafeae2f93648705d7
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-20 18:56:02 +0200

    code modified

--- lib/game.rb
+++ lib/game.rb
@@ -12,10 +12,12 @@ class Game
   def start
    # implement me and any other methods you feel like you need
    output.puts "Let's play a game. You will have to guess 1 anagram."
-   output.puts "1/1. Guess a word from an anagram #{Anagram.create("#{answers.join}")}"
+
+   output.puts "1/1. Guess a word from an anagram #{Anagram.create("#{answers[-1]}")}"
 
    evaluate_user_guesses
-  #  output.puts "2/2. Guess a word from an anagram #{Anagram.create("#{answers.last}")}"
+  # output.puts "2/2. Guess a word from an anagram #{Anagram.create("#{answers.last}")}"
+  # evaluate_user_guesses
   end
 
   def evaluate_user_guesses
@@ -24,27 +26,29 @@ class Game
     guess_counts = 1
     scores = 1
     max_attempts_count = 3
-    while guess != answers.join && current_attempt <= max_attempts
+    while guess != answers[-1] && current_attempt <= max_attempts
       guess = input.gets.to_s.strip
 
-        if guess == answers.join
-          output.puts "Correct! The answer is #{answers.join}."
+        if guess == answers[-1]
+          output.puts "Correct! The answer is #{answers[-1]}."
+        # elsif guess == answers[1]
+        #   output.puts "Correct! The answer is #{answers[-1]}."
         else
           output.puts "The answer is not #{guess}."
         end
 
-        if guess == answers.join && current_attempt <= max_attempts
+        if guess == answers[-1]  && current_attempt <= max_attempts
           output.puts "So far you have correctly guessed #{guess_counts} out of #{scores}."
           output.puts "Your final score: #{guess_counts}/#{scores}."
-        elsif guess != answers.join && current_attempt < max_attempts
+        elsif guess != answers[-1] && current_attempt < max_attempts
           output.puts "Try again. Attempts left: #{max_attempts_count -= 1}"
-        else guess != answers.join && current_attempt == max_attempts
+        else guess != answers[-1] && current_attempt == max_attempts
           output.puts "You lost."
           output.puts "Your final score: #{guess_counts -= 1}/#{scores}."
         end
       current_attempt += 1
     end
-    # if guess == answers.join && current_attempt <= max_attempts
+    # if guess == answers[-1] && current_attempt <= max_attempts
     #   output.puts "Your final score: #{guess_counts}/#{scores}."
     #   # output.puts "You lost."
     # if guess != answers.join && current_attempt > max_attempts
