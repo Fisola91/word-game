485aafca058c66e93c08595449e866cfc52e80c2
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-24 10:11:05 +0200

    The evaluate_user_guesses method is modified

--- lib/game.rb
+++ lib/game.rb
@@ -18,35 +18,35 @@ class Game
     end
     output.puts "Let's play a game. You will have to guess #{answers.size} #{word_anagram}."
     answers.each.with_index(1) do |answer, index|
-      p answer
+      # p answer
       output.puts "#{index}/#{answers.size}. Guess a word from an anagram #{Anagram.create("#{answer}")}"
-      evaluate_user_guesses
+      evaluate_user_guesses(answer)
     end
     output.puts "Your final score: #{correct_answers}/#{answers.size}."
   end
 
-  def evaluate_user_guesses
+  def evaluate_user_guesses(answer)
     guess = nil
     current_attempt = 1
-    answers.select do |answer|
-      while guess != answer && current_attempt <= max_attempts
-        guess = input.gets.to_s.strip
-        if guess == answer
-          output.puts "Correct! The answer is #{answer}."
-          @correct_answers += 1
-          output.puts "So far you have correctly guessed #{correct_answers} out of #{answers.size}."
-        else
-          output.puts "The answer is not #{guess}."
-          if current_attempt < max_attempts
-            output.puts "Try again. Attempts left: #{max_attempts - current_attempt}" # Generic approach that works for both cases
-          else
-            output.puts "You lost."
-          end
-        end
 
-        current_attempt += 1
+    while guess != answer && current_attempt <= max_attempts
+      guess = input.gets.to_s.strip
+      if guess == answer
+        output.puts "Correct! The answer is #{answer}."
+        @correct_answers += 1
+        output.puts "So far you have correctly guessed #{correct_answers} out of #{answers.size}."
+      else
+        output.puts "The answer is not #{guess}."
+        if current_attempt < max_attempts
+          output.puts "Try again. Attempts left: #{max_attempts - current_attempt}" # Generic approach that works for both cases
+        else
+          output.puts "You lost."
+        end
       end
+
+      current_attempt += 1
     end
+
 
   end
 
